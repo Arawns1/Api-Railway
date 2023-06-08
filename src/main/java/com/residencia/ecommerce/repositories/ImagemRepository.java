544c20775c39b0969314b1930e35ebac50440cbb
Gabriel Damico <gabrieldamico22@gmail.com>
2023-06-08 14:31:50 -0300

    api deletada

--- src/main/java/com/residencia/ecommerce/repositories/ImagemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.residencia.ecommerce.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.residencia.ecommerce.entites.Imagem;
-
-public interface ImagemRepository extends JpaRepository<Imagem, Integer> {
-
-}
